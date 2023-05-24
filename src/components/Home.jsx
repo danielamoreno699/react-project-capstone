2379ce14e445a454ec44f6e632c784cb85521821
Daniela Moreno <dani9292@hotmail.com>
2023-05-23 22:36:57 -0500

    Add provider to index.js

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/Home.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
 import { fetchData, selectStock } from '../redux/stockSlice';
@@ -51,7 +50,6 @@ const Home = () => {
       </div>
     </>
   );
-
-          };
+};
 
 export default Home;
