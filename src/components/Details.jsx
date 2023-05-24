b6c34f38b32b150fc92b8363fdc1e01a375ddc44
Daniela Moreno <dani9292@hotmail.com>
2023-05-23 22:43:22 -0500

    Add test files

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { BiArrowBack } from 'react-icons/bi';
 import { useNavigate, useParams } from 'react-router-dom';
