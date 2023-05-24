208247bb5bc8cf308054f86321a58479224f3452
Daniela Moreno <dani9292@hotmail.com>
2023-05-23 22:31:32 -0500

    Map the data of home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
 import { fetchData, selectStock } from '../redux/stockSlice';
 
-const Home = () => (
+const Home = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { data, status } = useSelector((state) => state.stocks);
@@ -21,7 +21,37 @@ const Home = () => (
     dispatch(selectStock(ticker));
     navigate(`/Details/${ticker}`);
   };
+  return (
+    <>
+      <div className="Container-box">
+        <div className="Container-txt">
+          <h1>Stock Market</h1>
+          <p>Here you can see the stocks</p>
+        </div>
+        <div className="p-txt-details">
+          <p>find your stocks and look for the details</p>
+        </div>
 
-);
+        <div className="Container-stocks">
+          {data.map((item) => (
+            <button
+              type="button"
+              className="Container-stocks-item"
+              key={uuid()}
+              onClick={() => onHandleSelect(item.ticker)}
+            >
+              <p className="ticker">
+                {item.ticker}
+              </p>
+              <p className="item-share">{item.cik}</p>
+
+            </button>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+
+          };
 
 export default Home;
