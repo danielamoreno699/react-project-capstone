d5e57976e29ffa8b6576f3cde2890de0d083b6ba
Daniela Moreno <dani9292@hotmail.com>
2023-05-23 22:29:42 -0500

    Add imports of files to home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import '../styles/Home.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { v4 as uuid } from 'uuid';
+import { fetchData, selectStock } from '../redux/stockSlice';
 
 const Home = () => (
   <div>Home</div>
--- src/redux/stockSlice.js
+++ src/redux/stockSlice.js
@@ -14,66 +14,66 @@ export const fetchData = createAsyncThunk('stocks/fetchData', async () => {
 
 // fetching details of stock passing the parameter of stock
 export const fetchStockInfo = createAsyncThunk(
-    'stocks/fetchStockInfo',
-    async (stock) => {
-      const response = await fetch(`https://api.polygon.io/v3/reference/tickers/${stock}?apiKey=Vp4PFtmRiJhA03_A8hWFal2787C86E4P`);
-      if (response.ok) {
-        const data = await response.json();
-        return data.results;
-      }
-      throw new Error('Failed to fetch stock information');
-    },
-  );
+  'stocks/fetchStockInfo',
+  async (stock) => {
+    const response = await fetch(`https://api.polygon.io/v3/reference/tickers/${stock}?apiKey=Vp4PFtmRiJhA03_A8hWFal2787C86E4P`);
+    if (response.ok) {
+      const data = await response.json();
+      return data.results;
+    }
+    throw new Error('Failed to fetch stock information');
+  },
+);
 
-  const initialState = {
-    status: 'idle',
-    data: [],
-    selectedStock: null,
-    stockInfo: {},
-  
-  };
-  
-  export const stockSlice = createSlice({
-    name: 'stocks',
-    initialState,
-    reducers: {
-      selectStock: (state, action) => {
-        const selectedStock = action.payload;
-        return {
-          ...state,
-          selectedStock,
-          stockInfo: {},
-        };
-      },
-    },
-    extraReducers: (builder) => {
-        builder
-          .addCase(fetchData.pending, (state) => {
-            state.status = 'loading';
-          })
-          .addCase(fetchData.fulfilled, (state, action) => ({
-            ...state,
-            status: 'succeeded',
-            data: action.payload,
-          }))
-          .addCase(fetchData.rejected, (state, action) => ({
-            ...state,
-            status: 'failed',
-            error: action.error.message,
-          }))
-          .addCase(fetchStockInfo.fulfilled, (state, action) => ({
-            ...state,
-            status: 'succeeded',
-            stockInfo: action.payload,
-          }))
-          .addCase(fetchStockInfo.rejected, (state, action) => ({
-    
-            ...state,
-            status: 'failed',
-            error: action.error.message,
-          }));
-      },
-    });
+const initialState = {
+  status: 'idle',
+  data: [],
+  selectedStock: null,
+  stockInfo: {},
 
-    export const { selectStock } = stockSlice.actions;
+};
+
+export const stockSlice = createSlice({
+  name: 'stocks',
+  initialState,
+  reducers: {
+    selectStock: (state, action) => {
+      const selectedStock = action.payload;
+      return {
+        ...state,
+        selectedStock,
+        stockInfo: {},
+      };
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchData.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchData.fulfilled, (state, action) => ({
+        ...state,
+        status: 'succeeded',
+        data: action.payload,
+      }))
+      .addCase(fetchData.rejected, (state, action) => ({
+        ...state,
+        status: 'failed',
+        error: action.error.message,
+      }))
+      .addCase(fetchStockInfo.fulfilled, (state, action) => ({
+        ...state,
+        status: 'succeeded',
+        stockInfo: action.payload,
+      }))
+      .addCase(fetchStockInfo.rejected, (state, action) => ({
+
+        ...state,
+        status: 'failed',
+        error: action.error.message,
+      }));
+  },
+});
+
+export const { selectStock } = stockSlice.actions;
 export default stockSlice.reducer;
